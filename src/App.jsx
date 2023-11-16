7203f387460a02437ff796a4c12ff2f33e854d76
shaimeur <haimeursaad@gmail.com>
2023-11-16 01:51:55 +0100

    implimentation of the basic route with no style

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
+import { BrowserRouter,Link,Route,Routes } from 'react-router-dom'
 
+
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
 import './App.css'
-
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
 
-    </>
+
+
+      <main>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+        </Routes>
+      </main>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+const About = ()=>{
+    return(
+        <div>
+            <h1>About</h1>
+            <p>Lear more about us!!</p>
+        </div>
+    )
+}
+
+export default About
