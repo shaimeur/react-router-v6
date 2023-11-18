0907c8d4ec952ee783e6bea0b3a037422e08ea85
shaimeur <haimeursaad@gmail.com>
2023-11-18 13:15:39 +0100

    adding tailwind css to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { BrowserRouter,Link,Route,Routes } from 'react-router-dom'
-
-
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
